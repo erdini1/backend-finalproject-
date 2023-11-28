91bada97963289d2beb7c92eef88de03d8c10d24
Dylan Erdini <erdini.dylan@gmail.com>
2023-11-28 00:33:44 -0300

    Agregando modifcación de cors

--- src/main/java/project/finalproject/usuario/UsuarioController.java
+++ src/main/java/project/finalproject/usuario/UsuarioController.java
@@ -24,6 +24,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UsuarioController {
 
     @Value("${jwt.secret}")
